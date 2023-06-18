9af476844afcb1de514f9c57fd62724bfc943169
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-18 20:34:51 -0300

    Limpeza de código

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
-import Footer from '@/src/patterns/Footer';
 import HomeScreen from '@/src/screens/HomeScreen';
 
 export default function Home() {
