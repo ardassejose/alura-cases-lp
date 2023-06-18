9cdbddfa4045458691b2784cef8b4fc46e051c91
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-18 20:28:24 -0300

    Adicionado Link e normalizado HomeScreen

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import Footer from '@/src/patterns/Footer';
+import HomeScreen from '@/src/screens/HomeScreen';
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <Link href="/faq">Ir para FAQ</Link>
+      <HomeScreen></HomeScreen>
       <Footer></Footer>
     </>
   );
