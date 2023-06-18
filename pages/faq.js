ffdcea8b687e274ff90d14266cfc083cc68e47c2
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-18 20:20:55 -0300

    Implementação de estilos, componentes e mais

---
+++ pages/faq.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function FAQ() {
+  return (
+    <>
+      <h1>FAQ</h1>
+      <Link href="/">Voltar</Link>
+    </>
+  );
+}
