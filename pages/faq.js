3c58a27e7ed5f2509b1604ba3e82be89402fb684
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-18 21:42:51 -0300

    Últimas edições

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import Head from "next/head";
 import Link from "next/link";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
@@ -17,6 +17,9 @@ export async function getStaticProps() {
 export default function FAQ({ faq }) {
   return (
     <>
+      <Head>
+        <title>FAQ - Alura Cases Campanha</title>
+      </Head>
       <h1>FAQ</h1>
       <Link href="/">Voltar</Link>
       <ul>
